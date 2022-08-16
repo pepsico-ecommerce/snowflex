3783580afec30c368498d1c9a67b9b961029bf44
Peter Morris <peter.morris@pepsico.com>
2022-08-16 10:09:24 -0400

    feat(type): Add module to enocde/decode queries
    We now handle the enocding and decoding of odbc sql types using the
    encode and decode behaviors on our DBConnection.Query implementation
    using a new `Type` module.

--- lib/snowflex/query.ex
+++ lib/snowflex/query.ex
@@ -1,4 +1,8 @@
 defmodule Snowflex.Query do
+  @moduledoc """
+  Implementation of `DBConnection.Query` for `Snowflex`.
+  """
+
   defstruct [
     :ref,
     :name,
@@ -9,9 +13,25 @@ defmodule Snowflex.Query do
   ]
 
   defimpl DBConnection.Query do
+    alias Snowflex.Result
+    alias Snowflex.Type
+
     def parse(query, _opts), do: query
     def describe(query, _opts), do: query
-    def encode(_query, params, _opts), do: params
+
+    @spec encode(query :: Query.t(), params :: [Type.param()], opts :: Keyword.t()) :: [
+            Type.param()
+          ]
+    def encode(_query, params, opts) do
+      Enum.map(params, &Type.encode(&1, opts))
+    end
+
+    @spec decode(query :: Query.t(), result :: Result.t(), opts :: Keyword.t()) :: Result.t()
+    def decode(_query, %Result{rows: rows} = result, opts) when not is_nil(rows) do
+      rows = Enum.map(rows, fn row -> Enum.map(row, &Type.decode(&1, opts)) end)
+      Map.put(result, :rows, rows)
+    end
+
     def decode(_query, result, _opts), do: result
   end
 
