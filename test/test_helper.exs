f4d056d7cd3181afebc7a2825b83d3c8df82ddc2
Peter Morris <peter.morris@pepsico.com>
2022-10-19 13:45:33 -0400

    feat(test): sqlite schema testing
    We add a migration generator module which provides a macro to generate the migrations needed to test any Ecto Schema.
    This allows you to swap in another Ecto adapter for local testing without connecting to Snowflake.
    
    To use this you can bring the Snowflex.SQLiteTestRepo into your project and use it as you normally would for testing.
    You will need to drop and create the repo before starting the tests each time so the migrations can work from a blank slate.
    
    You will also need to run the generate_migrations macro in your test_helper files with a list of all the Ecto Schemas you wish to test.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,18 @@
+require Snowflex.MigrationGenerator
+
+opts = [strategy: :one_for_one, name: Snowflex.Supervisor]
+Supervisor.start_link([Snowflex.SQLiteTestRepo], opts)
+
+Snowflex.SQLiteTestRepo.__adapter__().storage_up(Snowflex.SQLiteTestRepo.config())
+
+Snowflex.MigrationGenerator.generate_migrations(Snowflex.SQLiteTestRepo, [
+  TestSchema,
+  TestSchema2,
+  TestSchema3
+])
+
 ExUnit.start()
+
+ExUnit.after_suite(fn _ ->
+  Snowflex.SQLiteTestRepo.__adapter__().storage_down(Snowflex.SQLiteTestRepo.config())
+end)
