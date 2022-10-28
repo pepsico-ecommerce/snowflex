04f138f03724e96fea6b6bcdcc62c72985e9da0a
Peter Morris <peter.morris@pepsico.com>
2022-10-28 13:16:53 -0400

    feat(test): create and drop sqlite repo
    This allows for the SQLite test repo to automatically be created and deleted when running the test suite without any other changes.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,10 @@
 require Snowflex.MigrationGenerator
 
+opts = [strategy: :one_for_one, name: Snowflex.Supervisor]
+Supervisor.start_link([Snowflex.SQLiteTestRepo], opts)
+
+Snowflex.SQLiteTestRepo.__adapter__().storage_up(Snowflex.SQLiteTestRepo.config())
+
 Snowflex.MigrationGenerator.generate_migrations(Snowflex.SQLiteTestRepo, [
   TestSchema,
   TestSchema2,
@@ -7,3 +12,7 @@ Snowflex.MigrationGenerator.generate_migrations(Snowflex.SQLiteTestRepo, [
 ])
 
 ExUnit.start()
+
+ExUnit.after_suite(fn _ ->
+  Snowflex.SQLiteTestRepo.__adapter__().storage_down(Snowflex.SQLiteTestRepo.config())
+end)
