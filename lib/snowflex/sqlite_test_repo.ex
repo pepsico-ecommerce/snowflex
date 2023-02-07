ced21adc7d32ab1dbdaf07ba22ef880aa9795f9d
Dustin Farris <dustin.farris@pepsico.com>
2023-02-07 11:34:26 -0800

    refactor: only define test repo if sqlite3 is available
    This keeps the compiler happy if we're in a project or environment in
    which the user has chosen not to opt in to sqlite3 usage.

--- lib/snowflex/sqlite_test_repo.ex
+++ lib/snowflex/sqlite_test_repo.ex
@@ -1,5 +1,7 @@
-defmodule Snowflex.SQLiteTestRepo do
-  use Ecto.Repo,
-    otp_app: :snowflex,
-    adapter: Ecto.Adapters.SQLite3
+if Code.ensure_loaded?(Ecto.Adapters.SQLite3) do
+  defmodule Snowflex.SQLiteTestRepo do
+    use Ecto.Repo,
+      otp_app: :snowflex,
+      adapter: Ecto.Adapters.SQLite3
+  end
 end
