83a13306ffc3bea1f96ed493b4bd169b1c0afc24
Bruce Colby <bruce.colby.bc@gmail.com>
2023-10-23 12:19:41 -0400

    fix(snowflex-sqlite-test): typo in test name

--- test/snowflex_sqlite_test.exs
+++ test/snowflex_sqlite_test.exs
@@ -7,7 +7,7 @@ defmodule SnowflexSqliteTest do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
   end
 
-  test "can crete schema" do
+  test "can create schema" do
     Repo.insert!(%TestSchema{x: 1, y: 2, z: 3})
 
     assert [%TestSchema{x: 1, y: 2, z: 3}] = Repo.all(TestSchema)
