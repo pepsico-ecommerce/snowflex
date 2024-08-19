7c2ef5295eff0c9a651d015e4dea54e7b34c9f91
Bruce Colby <bruce.colby.bc@gmail.com>
2024-08-19 15:34:42 -0400

    Revert "Merge pull request #81 from pepsico-ecommerce/bc/dependency-fixes"
    This reverts commit 77a50be2795961b22524aa4735e872a62a8fad45, reversing
    changes made to b6eb28cc3578c5f9b2875e91315d9064d0f88e19.

--- lib/snowflex/migration_generator.ex
+++ lib/snowflex/migration_generator.ex
@@ -22,12 +22,15 @@ defmodule Snowflex.MigrationGenerator do
 
               create table(@source, primary_key: false) do
                 for {name, type} <- @fields do
-                  field_type = Ecto.Type.type(type)
+                  field_type = ecto_type_to_db_type(type)
                   field_source = name
                   add(field_source, type, primary_key: name in primary_keys)
                 end
               end
             end
+
+            defp ecto_type_to_db_type({:parameterized, Ecto.Enum, _}), do: :string
+            defp ecto_type_to_db_type(any), do: any
           end
 
           Ecto.Migrator.up(
@@ -52,13 +55,16 @@ defmodule Snowflex.MigrationGenerator do
                   type =
                     :type
                     |> @module.__schema__(field)
-                    |> Ecto.Type.type()
+                    |> ecto_type_to_db_type()
 
                   field_source = @module.__schema__(:field_source, field)
                   add(field_source, type, primary_key: field in primary_keys)
                 end
               end
             end
+
+            defp ecto_type_to_db_type({:parameterized, Ecto.Enum, _}), do: :string
+            defp ecto_type_to_db_type(any), do: any
           end
 
           Ecto.Migrator.up(
