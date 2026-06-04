86955b995fc17eca962ad5e7fd2b9e206573510e
Matthew Gilliam <chase.gilliam@gmail.com>
2026-06-04 16:22:49 -0400

    Fix dialyzer_ignore.exs formatting for Elixir 1.18 compat

--- dialyzer_ignore.exs
+++ dialyzer_ignore.exs
@@ -4,6 +4,12 @@
   # Pattern match on order_by direction that Dialyzer thinks is unreachable
   %{file: "lib/snowflex/ecto/adapter/connection.ex", code: :warn_matching},
   # :public_key functions removed from export in newer OTP but still available
-  %{file: "lib/snowflex/transport/http.ex", message: "Unknown function public_key:pem_decode/1"},
-  %{file: "lib/snowflex/transport/http.ex", message: "Unknown function public_key:pem_entry_decode/2"}
+  %{
+    file: "lib/snowflex/transport/http.ex",
+    message: "Unknown function public_key:pem_decode/1"
+  },
+  %{
+    file: "lib/snowflex/transport/http.ex",
+    message: "Unknown function public_key:pem_entry_decode/2"
+  }
 ]
