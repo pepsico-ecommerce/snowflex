d2174db8d0e059650b94d03a8dfbb23d72b33b77
Matthew Gilliam <chase.gilliam@gmail.com>
2026-07-13 10:06:00 -0400

    Bump version to 1.6.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Snowflex.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/pepsico-ecommerce/snowflex"
-  @version "1.5.0"
+  @version "1.6.0"
 
   def project do
     [
